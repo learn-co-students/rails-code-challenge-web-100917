bcfd74506a661c435f09668ab11a1acc8e896a28
MatteBru <mbruni616@gmail.com>
2017-11-10 12:23:36 -0500

    Danger - Experimental Autocomplete Inside

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,34 @@
 class SuperheroesController < ApplicationController
+  def search
+    @superheroes = Superhero.all.where(superpower: (Superpower.find_by(name: params[:search])))
+    render :index
+  end
+
   def index
     @superheroes = Superhero.all
   end
+
+  def show
+    @superhero = Superhero.find(params[:id])
+  end
+
+  def new
+    @superhero = Superhero.new
+  end
+
+  def create
+    @superhero = Superhero.new(hero_params)
+    # byebug
+    if @superhero.save
+      redirect_to superhero_path(@superhero)
+    else
+      render :new
+    end
+  end
+
+private
+
+def hero_params
+  params.require(:superhero).permit(:name, :super_name, :superpower_id)
+end
 end
--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,2 +1,7 @@
 class Superhero < ApplicationRecord
+  belongs_to :superpower
+  validates_presence_of :name
+  validates_presence_of :super_name
+  validates_uniqueness_of :super_name
+  # validates_presence_of :superpower
 end
