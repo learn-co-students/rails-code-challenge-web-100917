803a3a2ae38c17e975868bf4f2812e9d4aa5b51d
Dakota Lillie <dakota.lillie@icloud.com>
2017-11-10 12:20:02 -0500

    Dakota Lillie Done

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,54 @@
 class SuperheroesController < ApplicationController
+  before_action :set_superhero, only: [:show, :edit, :update]
+  
   def index
     @superheroes = Superhero.all
+  end
+  
+  def show
+  end
+  
+  def new
+    @superhero = Superhero.new
+  end
+  
+  def create
+    @superhero = Superhero.new(superhero_params)
+    if @superhero.save
+      redirect_to superhero_path(@superhero)
+    else
+      render :new
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @superhero.update(superhero_params)
+      redirect_to superhero_path(@superhero)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+  end
+  
+  def search
+    superpower = Superpower.find_by(name: params[:q])
+    @superheroes = superpower.superheroes
+    @message = "Superheroes with #{params[:q]}:"
+    render "index"
+  end
+  
+  private
+  
+  def set_superhero
+    @superhero = Superhero.find(params[:id])
+  end
+  
+  def superhero_params
+    params.require(:superhero).permit(:name, :super_name, :superpower_id)
   end
 end
--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,2 +1,5 @@
 class Superhero < ApplicationRecord
+  belongs_to :superpower
+  validates :name, uniqueness: true
+  validates :super_name, uniqueness: true
 end
