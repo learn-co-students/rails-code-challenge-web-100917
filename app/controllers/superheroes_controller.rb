4d7f81877181f63133d282472b96205a664c66f7
Gonzodamus <Dick@TheDickWard.com>
2017-11-10 12:20:30 -0500

    did many things but not all things

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,25 @@
 class SuperheroesController < ApplicationController
   def index
-    @superheroes = Superhero.all
+    if params[:search]
+      @superheroes = Superhero.all.find_all{|hero| hero.superpower.name == params[:search]}
+    else
+      @superheroes = Superhero.all
+    end
   end
+
+  def show
+    @superhero = Superhero.find(params[:id])
+  end
+
+  def new
+    @superhero = Superhero.new
+  end
+
+  def create
+  end
+
+  def superhero_params
+    params.require(:superhero).permit(:name, :super_name, :superpower_id)
+  end
+
 end
