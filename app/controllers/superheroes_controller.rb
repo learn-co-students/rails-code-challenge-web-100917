4a223ff0859e17b30f9eb83b94856995efcd5476
github username <github email address>
2017-11-10 12:21:43 -0500

    gveve commit

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -2,4 +2,28 @@ class SuperheroesController < ApplicationController
   def index
     @superheroes = Superhero.all
   end
+
+  def show
+    @superhero = Superhero.find(params[:id])
+  end
+
+  def new
+    @superhero = Superhero.new
+  end
+
+  def create
+    @superhero = Superhero.new(superhero_params)
+    if @superhero.save
+      render :show
+    else
+      render :new
+    end
+  end
+
+  private
+
+ def superhero_params
+    params.require(:superhero).permit(:name, :super_name, :superpower_id)
+ end
+
 end
--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,2 +1,6 @@
 class Superhero < ApplicationRecord
+  belongs_to :superpower
+
+  validates :super_name, presence: true, uniqueness: true
+
 end
