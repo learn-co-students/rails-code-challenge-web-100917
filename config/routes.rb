68f22dafe288f278322769a983d1d08128e21d58
github username <github email address>
2017-11-10 12:20:18 -0500

    my commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :superpowers, only: [:index]
-  resources :superheroes, only: [:index]
+  resources :superpowers, only: [:index, :show]
+  resources :superheroes, only: [:index, :show, :new, :create]
 
 end
--- db/migrate/20170725185425_create_superheroes.rb
+++ db/migrate/20170725185425_create_superheroes.rb
@@ -3,6 +3,7 @@ class CreateSuperheroes < ActiveRecord::Migration[5.0]
     create_table :superheroes do |t|
       t.string :name
       t.string :super_name
+      t.integer :superpower_id
 
       t.timestamps
     end
