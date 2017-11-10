c07c87905a6ebec627daad2c91e0687bf8e7b5e5
flored27 <dflores@fandm.edu>
2017-11-10 12:20:01 -0500

    done

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -2,4 +2,22 @@ class SuperheroesController < ApplicationController
   def index
     @superheroes = Superhero.all
   end
+
+  def show
+    @superhero = Superhero.find(params[:id])
+  end
+
+  def new
+    @superhero = Superhero.new
+  end
+
+  def create
+    @superhero = Superhero.new(params[:superhero])
+        if @superhero.save
+            redirect_to superhero_path(@superhero)
+        else
+            render 'new'
+        end
+    end
+
 end
