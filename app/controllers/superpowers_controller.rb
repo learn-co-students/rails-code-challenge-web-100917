0ef24d19d36272a0848bc60e062e67ed18ce6772
evaccaro <erv230@nyu.edu>
2017-11-10 12:20:17 -0500

    Elisa code challenge

--- app/controllers/superpowers_controller.rb
+++ app/controllers/superpowers_controller.rb
@@ -2,4 +2,15 @@ class SuperpowersController < ApplicationController
   def index
     @superpowers = Superpower.all
   end
+
+  def show
+    @superpower = Superpower.find(params[:id])
+  end
+
+
+  private
+
+  def superpower_params
+    params.require(:superpower).permit(:name, :description)
+  end
 end
