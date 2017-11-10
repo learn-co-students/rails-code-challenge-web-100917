9aa72c94be9af3fa58e6087fdaa37f9af8acec5d
github username <github email address>
2017-11-10 12:20:34 -0500

    Jordan's solution

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -2,4 +2,32 @@ class SuperheroesController < ApplicationController
   def index
     @superheroes = Superhero.all
   end
+
+  def show
+    @superhero = Superhero.find(params[:id])
+  end
+
+  def new
+    @superhero = Superhero.new
+  end
+
+  def create
+    @superhero = Superhero.new(super_params)
+    if @superhero.valid?
+      @superhero.save
+      redirect_to superhero_path(@superhero)
+    else
+      render :new
+    end
+  end
+
+  # def search
+  #   render :show
+  # end
+
+  private
+
+  def super_params
+    params.require(:superhero).permit(:name, :super_name, :superpower_id)
+  end
 end
